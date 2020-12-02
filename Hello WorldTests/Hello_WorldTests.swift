c4883e66f92e11137876be7ae21daadc96dc38be
Aman Shankar Sharma <sharma.s.aman@gmail.com>
2020-12-02 08:31:09 +0000

    Sample test action.

--- Hello WorldTests/Hello_WorldTests.swift
+++ Hello WorldTests/Hello_WorldTests.swift
@@ -22,6 +22,7 @@ class Hello_WorldTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(ViewController().returnTrue())
     }
 
     func testPerformanceExample() {
