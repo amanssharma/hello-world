0d8b31c1c1cf562af886fd9ab4c27494ac6093ba
Aman Shankar Sharma <sharma.s.aman@gmail.com>
2020-12-03 08:20:45 +0000

    PR trigger. (#3)
    * PR trigger.
    
    * Setup pull requests based workflows and added command options to generate HTML reports.

--- .github/scripts/test-dev.sh
+++ .github/scripts/test-dev.sh
@@ -2,7 +2,7 @@ set -eo pipefail
 
 xcodebuild	clean test\
 			-workspace "Hello World.xcworkspace"\
-			-scheme $1\
+			-scheme dev\
 			-destination 'platform=iOS Simulator,name=iPhone 12 Pro'\
 			-derivedDataPath build/derived-data\
 			-enableCodeCoverage YES | xcpretty --report html
