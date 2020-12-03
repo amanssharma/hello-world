d331d8e25f7fb20732168fc59543b1194a19c3b1
Aman Shankar Sharma <sharma.s.aman@gmail.com>
2020-12-03 08:47:47 +0000

    Removed inaccurate / trivial comments.

--- .github/scripts/init-submodules.sh
+++ .github/scripts/init-submodules.sh
@@ -1,2 +1,3 @@
 set -eo pipefail
+
 git submodule init
--- .github/scripts/slather-out.sh
+++ .github/scripts/slather-out.sh
@@ -1,3 +1,4 @@
 set -eo pipefail
+
 gem install slather
 slather coverage --html --output-directory build/reports/coverage-reports --scheme dev --binary-basename "Hello World" --build-directory "build" --workspace "Hello World.xcworkspace" Hello\ World.xcodeproj
